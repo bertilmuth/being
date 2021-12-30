e00a5a3cb7443740c4c33fc78e970b7f4882b941
Bertil Muth <bertilmuth@yahoo.de>
2021-12-31 00:06:16 +0100

    Create CommandModelJournalProvider.java

---
+++ being-vlingo/src/main/java/org/requirementsascode/being/CommandModelJournalProvider.java
@@ -0,0 +1,46 @@
+package org.requirementsascode.being;
+
+import java.util.Arrays;
+
+import org.requirementsascode.being.EventSourcedAggregateBehavior;
+
+import io.vlingo.xoom.actors.Stage;
+import io.vlingo.xoom.lattice.model.sourcing.SourcedTypeRegistry;
+import io.vlingo.xoom.lattice.model.sourcing.SourcedTypeRegistry.Info;
+import io.vlingo.xoom.symbio.store.dispatch.Dispatcher;
+import io.vlingo.xoom.symbio.store.dispatch.NoOpDispatcher;
+import io.vlingo.xoom.symbio.store.journal.Journal;
+import io.vlingo.xoom.turbo.ComponentRegistry;
+import io.vlingo.xoom.turbo.actors.Settings;
+import io.vlingo.xoom.turbo.annotation.persistence.Persistence.StorageType;
+import io.vlingo.xoom.turbo.storage.Model;
+import io.vlingo.xoom.turbo.storage.StoreActorBuilder;
+
+@SuppressWarnings("all")
+public class CommandModelJournalProvider  {
+
+  public final Journal<String> journal;
+
+  public static CommandModelJournalProvider using(final Stage stage, final SourcedTypeRegistry registry) {
+    return using(stage, registry, new NoOpDispatcher());
+ }
+
+  public static CommandModelJournalProvider using(final Stage stage, final SourcedTypeRegistry registry, final Dispatcher ...dispatchers) {
+    if (ComponentRegistry.has(CommandModelJournalProvider.class)) {
+      return ComponentRegistry.withType(CommandModelJournalProvider.class);
+    }
+
+    final Journal<String> journal =
+              StoreActorBuilder.from(stage, Model.COMMAND, Arrays.asList(dispatchers), StorageType.JOURNAL, Settings.properties(), true);
+
+    registry.register(new Info(journal, EventSourcedAggregateBehavior.class, EventSourcedAggregateBehavior.class.getSimpleName()));
+
+    return new CommandModelJournalProvider(journal);
+  }
+
+  private CommandModelJournalProvider(final Journal<String> journal) {
+    this.journal = journal;
+    ComponentRegistry.register(getClass(), this);
+  }
+
+}
