7be9f057e94d4ce61764d685823f4d980e6a5175
Bertil Muth <bertilmuth@yahoo.de>
2022-01-01 18:11:31 +0100

    Add projection

---
+++ being-vlingo/src/main/java/org/requirementsascode/being/ProjectionActor.java
@@ -0,0 +1,54 @@
+package org.requirementsascode.being;
+
+import io.vlingo.xoom.lattice.model.projection.Projectable;
+import io.vlingo.xoom.lattice.model.projection.StateStoreProjectionActor;
+import io.vlingo.xoom.symbio.Source;
+import io.vlingo.xoom.symbio.store.state.StateStore;
+import io.vlingo.xoom.turbo.ComponentRegistry;
+
+/**
+ * See <a href=
+ * "https://docs.vlingo.io/xoom-lattice/projections#implementing-with-the-statestoreprojectionactor">
+ * StateStoreProjectionActor </a>
+ * @param <DATA> the data type of the projected view model
+ */
+public class ProjectionActor<DATA> extends StateStoreProjectionActor<DATA> {
+	private final ViewModel<DATA> viewModel;
+
+	public <U> ProjectionActor(ViewModel<DATA> viewModel) {
+		this(ComponentRegistry.withType(QueryModelStateStoreProvider.class).store, viewModel);
+	}
+
+	public ProjectionActor(StateStore store, ViewModel<DATA> viewModel) {
+		super(store);
+		this.viewModel = viewModel;
+	}
+
+	@Override
+	protected DATA currentDataFor(final Projectable projectable) {
+		return viewModel.emptyData();
+	}
+
+	@Override
+	protected DATA merge(final DATA previousData, final int previousVersion,
+			final DATA currentData, final int currentVersion) {
+
+		if (previousVersion == currentVersion)
+			return currentData;
+
+		DATA dataToMerge = previousData == null? currentData : previousData;
+
+		for (final Source<?> event : sources()) {
+			DATA mergedData = viewModel.merge(dataToMerge, event);
+			
+			if(dataToMerge == mergedData) {
+				logger().warn("Event of type " + event.typeName() + " was not matched.");
+				break;
+			} else {
+				dataToMerge = mergedData;
+			}
+		}
+
+		return dataToMerge;
+	}
+}
