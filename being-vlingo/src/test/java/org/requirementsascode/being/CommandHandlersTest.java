16602a4502219f7eb275eb2783dfe342ac4f48ea
Bertil Muth <bertilmuth@yahoo.de>
2022-01-09 20:45:00 +0100

    Update CommandHandlersTest.java

--- being-vlingo/src/test/java/org/requirementsascode/being/CommandHandlersTest.java
+++ being-vlingo/src/test/java/org/requirementsascode/being/CommandHandlersTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.requirementsascode.being.CommandHandler.commandsOf;
 
 import java.util.List;
-import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,50 +12,49 @@ import io.vlingo.xoom.lattice.model.IdentifiedDomainEvent;
 
 class CommandHandlersTest {
 	@Test
-	void createsEmptyCommandMappers() {
-		CommandHandlers<TestCommand> mapCommands = CommandHandlers.with();
-		
-		List<Class<? extends TestCommand>> commands = mapCommands.getCommandClasses();
+	void createsEmptyCommandHandlers() {
+		CommandHandlers<TestCommand> commandHandlers = CommandHandlers.with();
+
+		List<Class<? extends TestCommand>> commands = commandHandlers.getCommandClasses();
 		assertTrue(commands.isEmpty());
 	}
 
 	@Test
 	void createsOneCommandMapper() {
-		final Function<SampleCommand1, SampleEvent1> handler = command -> new SampleEvent1(command.id);
-		CommandHandlers<TestCommand> mapCommands = CommandHandlers.with(
-			commandsOf(SampleCommand1.class).toEvent(handler)
+		CommandHandlers<TestCommand> commandHandlers = CommandHandlers.with(
+			commandsOf(SampleCommand1.class).toEvent(command -> new SampleEvent1(command.id))
 		);
-		
-		List<Class<? extends TestCommand>> commandClasses = mapCommands.getCommandClasses();
+
+		List<Class<? extends TestCommand>> commandClasses = commandHandlers.getCommandClasses();
 		assertEquals(1, commandClasses.size());
 		assertEquals(SampleCommand1.class, commandClasses.get(0));
 	}
-	
-	@Test
-	void createsTwoCommandMappers() {
-		final Function<SampleCommand1, SampleEvent1> handler1 = command -> new SampleEvent1(command.id);
-		CommandHandler<SampleCommand1> mapCommand1 = commandsOf(SampleCommand1.class).toEvent(handler1);
-		final Function<SampleCommand2, SampleEvent2> handler2 = command -> new SampleEvent2(command.id);
-		CommandHandler<SampleCommand2> mapCommand2 = commandsOf(SampleCommand2.class).toEvent(handler2);
 
-		CommandHandlers<TestCommand> mapCommands = CommandHandlers.with(mapCommand1, mapCommand2);
-		List<Class<? extends TestCommand>> commandClasses = mapCommands.getCommandClasses();
+	@Test
+	void createsTwoCommandHandlers() {
+		CommandHandlers<TestCommand> commandHandlers = CommandHandlers.with(
+			commandsOf(SampleCommand1.class).toEvent(command -> new SampleEvent1(command.id)),
+			commandsOf(SampleCommand2.class).toEvent(command -> new SampleEvent2(command.id))
+		);
+		
+		List<Class<? extends TestCommand>> commandClasses = commandHandlers.getCommandClasses();
 		assertEquals(2, commandClasses.size());
 		assertEquals(SampleCommand1.class, commandClasses.get(0));
 		assertEquals(SampleCommand2.class, commandClasses.get(1));
 	}
-	
-	private interface TestCommand{}
-	
-	private class SampleCommand1 implements TestCommand{
-		public String id;
-	};
-	
-	private class SampleCommand2 implements TestCommand{
+
+	private interface TestCommand {
+	}
+
+	private class SampleCommand1 implements TestCommand {
 		public String id;
 	};
 
-	private class SampleEvent1 extends IdentifiedDomainEvent{
+	private class SampleCommand2 implements TestCommand {
+		public String id;
+	};
+
+	private class SampleEvent1 extends IdentifiedDomainEvent {
 		public String id;
 
 		public SampleEvent1(String id) {
@@ -68,10 +66,10 @@ class CommandHandlersTest {
 			return id;
 		}
 	};
-	
-	private class SampleEvent2 extends IdentifiedDomainEvent{
+
+	private class SampleEvent2 extends IdentifiedDomainEvent {
 		public String id;
-		
+
 		public SampleEvent2(String id) {
 			this.id = id;
 		}
