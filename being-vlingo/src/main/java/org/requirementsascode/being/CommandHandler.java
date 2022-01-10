43116991f5a19e02a0ec3c9abc289357c210befa
Bertil Muth <bertilmuth@yahoo.de>
2022-01-10 16:35:45 +0100

    Update CommandHandler.java

--- being-vlingo/src/main/java/org/requirementsascode/being/CommandHandler.java
+++ being-vlingo/src/main/java/org/requirementsascode/being/CommandHandler.java
@@ -9,7 +9,7 @@ import io.vlingo.xoom.lattice.model.IdentifiedDomainEvent;
 
 public class CommandHandler<CMD>{
 	private final Class<CMD> commandClass;
-	private final Function<CMD, List<? extends IdentifiedDomainEvent>> mapFunction;
+	private final Function<CMD, List<? extends IdentifiedDomainEvent>> commandHandler;
 
 	public static <T> CommandsOf<T> commandsOf(Class<T> commandClass) {
 		return new CommandsOf<T>(commandClass);
@@ -22,28 +22,28 @@ public class CommandHandler<CMD>{
 			this.commandClass = commandClass;
 		}
 
-		public CommandHandler<T> with(Function<T, ? extends IdentifiedDomainEvent> mapFunction) {
+		public CommandHandler<T> with(Function<T, ? extends IdentifiedDomainEvent> commandHandler) {
 			Function<T, List<? extends IdentifiedDomainEvent>> eventListProducingHandler = cmd -> {
-				IdentifiedDomainEvent result = mapFunction.apply(cmd);
+				IdentifiedDomainEvent result = commandHandler.apply(cmd);
 				return Collections.singletonList(result);
 			};
 			
 			return withSome(eventListProducingHandler);
 		}
 
-		public CommandHandler<T> withSome(Function<T, List<? extends IdentifiedDomainEvent>> mapFunction) {
-			return new CommandHandler<>(commandClass, mapFunction);
+		public CommandHandler<T> withSome(Function<T, List<? extends IdentifiedDomainEvent>> commandHandler) {
+			return new CommandHandler<>(commandClass, commandHandler);
 		}
 	}
 	
-	private CommandHandler(Class<CMD> commandClass, Function<CMD, List<? extends IdentifiedDomainEvent>> mapFunction) {
+	private CommandHandler(Class<CMD> commandClass, Function<CMD, List<? extends IdentifiedDomainEvent>> commandHandler) {
 		this.commandClass = Objects.requireNonNull(commandClass, "commandClass must be non-null!");
-		this.mapFunction = Objects.requireNonNull(mapFunction, "mapFunction must be non-null!");
+		this.commandHandler = Objects.requireNonNull(commandHandler, "commandHandler must be non-null!");
 	}
 	
 	@SuppressWarnings("unchecked")
 	List<? extends IdentifiedDomainEvent> reactTo(Object command) {
-		return mapFunction.apply((CMD)command);
+		return commandHandler.apply((CMD)command);
 	}
 	
 	public Class<CMD> getCommandClass() {
