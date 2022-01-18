02c3cd9c4ec9ae36ce034f3a49c8f89458e48e35
Bertil Muth <bertilmuth@yahoo.de>
2022-01-18 23:15:53 +0100

    Update Beings.java

--- being-core/src/main/java/org/requirementsascode/being/Beings.java
+++ being-core/src/main/java/org/requirementsascode/being/Beings.java
@@ -5,7 +5,22 @@ import io.vlingo.xoom.actors.Stage;
 import io.vlingo.xoom.lattice.model.sourcing.SourcedTypeRegistry;
 import io.vlingo.xoom.symbio.store.dispatch.Dispatcher;
 
+/**
+ * Class for setting up and managing the infrastructure that the Being library is based on.
+ * 
+ * @author b_muth
+ *
+ */
 public class Beings {
+	
+	/**
+	 * Sets up and starts up persistence for the aggregate behavior model and query model. 
+	 * The events are persisted in a journal, and the query model is persisted using a state store.
+	 * After calling this method, the aggregate behavior model and query model are available as actors.
+	 * 
+	 * @param stage the stage where the actors for the command model and query model are created in
+	 * @param queryModel the query model that needs to be connected to the command handling model
+	 */
 	@SuppressWarnings("rawtypes")
 	public static void baseOn(Stage stage, QueryModel<?> queryModel) {
 		final Stage defaultStage = stage.world().stage();
